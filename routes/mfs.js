8891fd637e7316a029e2f16a7803c264ddef98e1
岳小云 <sfreee@gmail.com>
2017-01-19 10:58:06 +0800

    测试

--- routes/mfs.js
+++ routes/mfs.js
@@ -7,6 +7,11 @@ const upload = multer({
     dest: '/data/mfs/'
 });
 
+function getExtension(fpath) {
+    var split = fpath.split(".");
+    return split[split.length - 1];
+}
+
 var router = new Router();
 
 router.get('/', function(ctx, next) {
@@ -21,11 +26,23 @@ router.post('*', upload.single('file'), function*() {
     console.log(this.params);
     // console.log(this.request.body.files);
     console.log(this.request.body.files);
-    // console.log(this.request.body.files);
-    // if (this.request.body.files.file) {
-    //     //fs.copy()
-    // }
-    this.body = 'upload';
+
+    var file = this.request.body.files.file;
+    var extension = getExtension(file.name);
+
+    var data = {
+        "result": 0,
+        "errorCode": 0,
+        "msg": "SUCCESS",
+        "info": {
+            "url": "http://mfs-bak.oss-cn-beijing.aliyuncs.com/" + file.path.substring(10), //upload_0bf69031530ec1f342dcce3155b83eec.xlsx
+            "filename": file.name,
+            "md5filename": "70ad8ccc7db24bfb503814de0adc21c7.jpg",
+            "suffix": "." + extension,
+            "filesize": 61293
+        }
+    };
+    this.body = data;
 });
 
 module.exports = router;
